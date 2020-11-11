579be4a3e62b42711b537cce969ecb7a86dfa4e0
yemecen <y.emecen@gmail.com>
2020-11-11 16:37:01 +0300

    Ders-4

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,20 +1,39 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import Employee from './Employee';
+import { Button, Modal } from 'react-bootstrap';
 import { EmployeeContext } from '../contexts/EmployeeContext';
+import AddForm from './AddForm';
 
 const EmployeeList = () => {
 
-    const {employees} = useContext(EmployeeContext);
+    const {employees} = useContext(EmployeeContext)
+
+    const [show, setShow] = useState(false)
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true)
 
     return (
+
+        <>
+        <div className="table-title">
+        <div className="row">
+          <div className="col-sm-6">
+            <h2>Manage <b>Employees</b></h2>
+          </div>
+          <div className="col-sm-6">
+            <Button onClick={handleShow} className="btn btn-success text-white" data-toggle="modal"><i className="material-icons">&#xE147;</i> <span>Add New Employee</span></Button>
+          </div>
+        </div>
+      </div>
 
         <table className="table table-striped table-hover">
             <thead>
                 <tr>
                     <th>Name</th>
-                    <th>Mail</th>
+                    <th>Email</th>
                     <th>Address</th>
-                    <th>Phole</th>
+                    <th>Phone</th>
                     <th>Actions</th>
                 </tr>
             </thead>
@@ -23,6 +42,22 @@ const EmployeeList = () => {
             </tbody>
         </table>
 
+        <Modal show={show} onHide={handleClose}>
+            <Modal.Header className="modal-header" closeButton>
+                <Modal.Title>
+                    Add Employee
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <AddForm />
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                    Close Modal
+                </Button>
+            </Modal.Footer>
+        </Modal>
+        </>
     )
 }
 
